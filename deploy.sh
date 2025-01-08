49b2fc9255667f8a1e0c8878917d8997384629f6
ayush singh <96005099+ayush-7714@users.noreply.github.com>
2025-01-08 13:20:32 +0530

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+  GNU nano 8.2                   deploy.sh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                              [ New File ]
+^G Help       ^O Write Out  ^F Where Is   ^K Cut        ^T Execute
+^X Exit       ^R Read File  ^\ Replace    ^U Paste      ^J Justify
